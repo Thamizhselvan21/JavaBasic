2bf687b116d3aedf684565ed2d5c86643d8f4eb6
TNS INDIA <TNS INDIA@Thamizh-TNS>
2024-06-27 11:03:05 +0530

    This is my core java project

---
+++ JavaBasic/src/Day1/Sample.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to JFS");
+		System.out.println("MVIT");
+		System.out.print("TNSIF");
+	}
+
+}
